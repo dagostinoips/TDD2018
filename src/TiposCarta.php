bfe49a2940c50623c8c87ba14ebde3a3aa441aec
Emmanuel M <emmanuelbendavid@gmail.com>
2018-09-19 19:58:21 -0300

    Added TiposCarta

---
+++ src/TiposCarta.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TDD;
+
+
+abstract class TiposCarta {
+    const E_ORO = "eo";
+    const E_COPA = "ec";
+    const E_ESPADA = "ee";
+    const E_BASTO = "eb";
+
+    const P_TREBOL = "pt";
+    const P_PICA = "pp";
+    const P_CORAZON = "pc";
+    const P_DIAMANTE = "pd";
+}
