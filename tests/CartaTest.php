6ea3cb02bbffed8e4629461c26a8f7bb0f4538e6
joeltorboli <joeltorboli@gmail.com>
2018-09-20 16:13:54 -0300

    Agregado test numero

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -7,8 +7,13 @@ use PHPUnit\Framework\TestCase;
 class CartaTest extends TestCase {
 
     public function testPalo() {
-        $carta = New Carta("Espada");
+        $carta = New Carta(0, "Espada");
         $this->assertEquals($carta->palo(), "Espada");
+    }
+
+    public function testNumero() {
+        $carta = New Carta(11, "Basto");
+        $this->assertEquals($carta->numero(), 11);
     }
 
 }
