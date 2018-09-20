15a477d8369ec64bc588351e2abfed40e37c7af1
joeltorboli <joeltorboli@gmail.com>
2018-09-20 15:53:35 -0300

    Agregado test palo

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -5,5 +5,10 @@ namespace TDD;
 use PHPUnit\Framework\TestCase;
 
 class CartaTest extends TestCase {
-    
+
+    public function testPalo() {
+        $carta = New Carta(0,"Espada");
+        $this->assertEquals($carta->palo(), "Espada");
+    }
+
 }
